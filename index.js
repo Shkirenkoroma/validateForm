aa73ce288eb5848ddef992053b25bc7daff052de
Shkirenkoroma <shkirenkoroma@mail.ru>
2023-04-01 23:38:52 +0300

    added new feature

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ inputName.addEventListener("blur", getRequiredField);
 inputName.addEventListener("focus", putDataName);
 
 function putDataName() {
-	inputName.value = "";
+	if (
+		inputName.value === "Обязательное поле" ||
+		inputName.value === "Допустимый диапазон от 2 до 25 символов"
+	) {
+		inputName.value = "";
+	}
 	inputName.className = "usuallyState";
 }
 
@@ -11,5 +16,32 @@ function getRequiredField() {
 	if (inputName.value === "") {
 		inputName.className = "required";
 		inputName.value = "Обязательное поле";
+	} else if (inputName.value.length < 2 || inputName.value.length > 25) {
+		inputName.className = "required";
+		inputName.value = "Допустимый диапазон от 2 до 25 символов";
+	}
+}
+
+const inputSurName = document.getElementById("surname");
+inputSurName.addEventListener("blur", getField);
+inputSurName.addEventListener("focus", putDataSurName);
+
+function putDataSurName() {
+	if (
+		inputSurName.value === "Обязательное поле" ||
+		inputSurName.value === "Допустимый диапазон от 2 до 25 символов"
+	) {
+		inputSurName.value = "";
+	}
+	inputSurName.className = "usuallyState";
+}
+
+function getField() {
+	if (inputSurName.value === "") {
+		inputSurName.className = "required";
+		inputSurName.value = "Обязательное поле";
+	} else if (inputSurName.value.length < 2 || inputSurName.value.length > 25) {
+		inputSurName.className = "required";
+		inputSurName.value = "Допустимый диапазон от 2 до 25 символов";
 	}
 }
